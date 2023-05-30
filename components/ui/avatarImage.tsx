c803bc01a25bf98dbe1bffa844a7816969dbfa0d
Kayle54187 <habichristi@gmail.com>
2023-05-30 20:14:04 +0200

    Removing Status from AvatarImageProps Interface Using data from Props

--- components/ui/avatarImage.tsx
+++ components/ui/avatarImage.tsx
@@ -2,12 +2,8 @@ import AvatarImageProps from "@/Interfaces/AvatarImageProps"
 
 
 
-export default function AvatarImage(props: AvatarImageProps){
-    const data = {
-        src: 'https://images.unsplash.com/photo-1482361046637-0226fdcfa3b7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80',
-        alt: 'Girl'
-    }
+export default function AvatarImage({ src, alt }: AvatarImageProps){
     return(
-        <img src={data.src} className="w-10 h-10 rounded-full object-cover"/>
+        <img src={src} className="w-10 h-10 rounded-full object-cover" alt={alt}/>
     )
 }
