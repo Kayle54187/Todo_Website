cc4334191e1e73fa45b9c46e84ce0418a4c21402
Kayle54187 <habichristi@gmail.com>
2023-06-03 22:32:38 +0200

    Using Functional Component Generic

--- components/ui/avatarImage.tsx
+++ components/ui/avatarImage.tsx
@@ -2,8 +2,10 @@ import AvatarImageProps from "@/Interfaces/AvatarImageProps"
 
 
 
-export default function AvatarImage({ src, alt }: AvatarImageProps){
+const AvatarImage: React.FC<AvatarImageProps> = ({ src, alt }) => {
     return(
         <img src={src} className="w-10 h-10 rounded-full object-cover" alt={alt}/>
     )
 }
+
+export default AvatarImage
