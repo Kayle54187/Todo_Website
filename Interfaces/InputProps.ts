7e356c9b46b97217a0bfac5f3bfbcc2b07bb3de9
Kayle54187 <habichristi@gmail.com>
2023-06-03 22:44:01 +0200

    value name update

--- Interfaces/InputProps.ts
+++ Interfaces/InputProps.ts
@@ -2,7 +2,7 @@ import React, { ChangeEvent } from "react";
 
 export default interface InputProps {
     Placeholder: string,
-    Value?: string,
+    value?: string,
     Icon?: React.ElementType,
     is_icon: boolean,
     status?: "Primary" | "Idle" | "Error",
