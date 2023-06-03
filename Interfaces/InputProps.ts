91228b2374f92d48124cac6fec051cf30aa2aa31
Kayle54187 <habichristi@gmail.com>
2023-06-03 22:36:11 +0200

    Adding Icon States Adding onChange property Adding Value property

--- Interfaces/InputProps.ts
+++ Interfaces/InputProps.ts
@@ -1,4 +1,10 @@
+import React, { ChangeEvent } from "react";
+
 export default interface InputProps {
     Placeholder: string,
-    Icon: React.ReactNode
+    Value?: string,
+    Icon?: React.ElementType,
+    is_icon: boolean,
+    status?: "Primary" | "Idle" | "Error",
+    onChange: (e: ChangeEvent<HTMLInputElement>)=>void
 }
